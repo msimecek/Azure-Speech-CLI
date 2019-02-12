04f32a3735fa7bd6d2a7815636c28508cc8f981e
Martin Simecek <msimecek@users.noreply.github.com>
2019-02-12 13:46:43 +0100

    Version up + script for NuGet

---
+++ SpeechCLI/pack-nuget.ps1
@@ -0,0 +1,1 @@
+dotnet pack ./SpeechCLI.csproj -c Release /p:Description="Unofficial CLI tool for Microsoft Azure Speech service management - datasets%44 models%44 tests%44 endpoints etc. Useful for automation." /p:Title="Azure Speech CLI" /p:PackAsTool=true
