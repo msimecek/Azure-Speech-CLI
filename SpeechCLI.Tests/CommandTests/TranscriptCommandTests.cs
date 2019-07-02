1f8f4ada0e256a40d449a69cf9cdf69e86322570
Martin Simecek <msimecek@users.noreply.github.com>
2019-07-02 16:46:36 +0200

    First test

--- SpeechCLI.Tests/CommandTests/TranscriptCommandTests.cs
+++ SpeechCLI.Tests/CommandTests/TranscriptCommandTests.cs
@@ -8,11 +8,83 @@ using Xunit;
 using CRIS.Models;
 using McMaster.Extensions.CommandLineUtils;
 using System.Threading.Tasks;
+using System.Net.Http;
+using Moq.Protected;
+using System.Threading;
+using System.Net;
+using Microsoft.Extensions.DependencyInjection;
+using SpeechCLI.Tests.Utils;
+using System.Diagnostics;
+using WireMock.Server;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using SpeechCLI.Interfaces;
+using Microsoft.Rest;
 
 namespace SpeechCLI.Tests.CommandTests
 {
     public class TranscriptCommandTests
     {
-        
+        public CommandLineApplication<MainApp> InitApp(ISpeechServicesAPIv20 apiObject)
+        {
+            var services = new ServiceCollection()
+                .AddSingleton<IConfig, Config>()
+                .AddSingleton<ISpeechServicesAPIv20>(apiObject)
+                .BuildServiceProvider();
+
+            var writer = new MockTestWriter();
+            var app = new CommandLineApplication<MainApp>(new MockConsole(writer));
+            app.Conventions.UseDefaultConventions().UseConstructorInjection(services);
+
+            return app;
+        }
+
+        static string[] CommandIntoArgs(string command)
+        {
+            return command.Split(' ');
+        }
+
+        [Fact]
+        public void List_Success()
+        {
+            // ARRANGE
+            var response = new List<Transcription>()
+            {
+                new Transcription()
+                {
+                    Id = Guid.Empty,
+                    Name = "Moq",
+                    Status = SpeechCLI.Utils.Constants.SUCCEEDED_STATUS
+                }
+            };
+
+            var mock = new Mock<ISpeechServicesAPIv20>();
+            mock
+                .Setup(
+                    m => m.GetTranscriptionsWithHttpMessagesAsync(null, CancellationToken.None)
+                )
+                .ReturnsAsync(
+                    new HttpOperationResponse<object, GetTranscriptionsHeaders>() { Body = response }
+                );
+
+            //var mockServer = FluentMockServer.Start();
+            //mockServer
+            //    .Given(Request.Create().WithPath("/api/speechtotext/v2.0/transcriptions").UsingGet())
+            //    .RespondWith(Response.Create().WithStatusCode(200).WithBody("OK"));
+
+            //var hc = new HttpClient();
+            //hc.BaseAddress = new Uri($"http://localhost:{mockServer.Ports[0]}");
+            //var sdk = new SpeechServicesAPIv20(hc, true);
+
+            var app = InitApp(mock.Object);
+
+            // ACT
+            var args = CommandIntoArgs("transcript list");
+            app.Execute(args);
+
+            // ASSES
+            Assert.Equal("Getting transcriptions...\r\n00000000-0000-0000-0000-000000000000 Moq Succeeded\r\n", ((MockTestWriter)app.Out).ReadAsString());
+        }
+
     }
 }
---
+++ SpeechCLI.Tests/Utils/MockConsole.cs
@@ -0,0 +1,86 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace SpeechCLI.Tests.Utils
+{
+    public class MockConsole : IConsole
+    {
+        public MockConsole(TextWriter outputWriter)
+        {
+            Out = outputWriter;
+            Error = outputWriter;
+        }
+
+        public TextWriter Out { get; set; }
+
+        public TextWriter Error { get; set; }
+
+        public TextReader In => throw new NotImplementedException();
+
+        public bool IsInputRedirected => throw new NotImplementedException();
+
+        public bool IsOutputRedirected => true;
+
+        public bool IsErrorRedirected => true;
+
+        public ConsoleColor ForegroundColor { get; set; }
+        public ConsoleColor BackgroundColor { get; set; }
+
+        public event ConsoleCancelEventHandler CancelKeyPress
+        {
+            add { }
+            remove { }
+        }
+
+        public void ResetColor()
+        {
+        }
+
+        
+    }
+
+    public class MockTestWriter : TextWriter
+    {
+        private StringBuilder _sb = new StringBuilder();
+
+        public override Encoding Encoding => Encoding.Unicode;
+
+        public override void Write(char ch)
+        {
+            //if (ch == '\n')
+            //{
+            //    //_output.WriteLine(_sb.ToString());
+            //    _sb.Clear();
+            //}
+            //else
+            //{
+            //    _sb.Append(ch);
+            //}
+
+            _sb.Append(ch);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_sb.Length > 0)
+                {
+                    //_output.WriteLine(_sb.ToString());
+                    _sb.Clear();
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+
+        public string ReadAsString()
+        {
+            return _sb.ToString();
+        }
+    }
+}
